84548f2f4a3cee18b1b271db81b097e393e59fe0
R0ryMercury <12345hasek@gmail.com>
2022-09-17 21:49:14 +0300

    added requirements.txt

--- project/dao/model/genre.py
+++ project/dao/model/genre.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields
 
 
 class Genre(db.Model):
-    __tablename__ = "director"
+    __tablename__ = "genre"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
 
